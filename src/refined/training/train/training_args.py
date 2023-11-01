743c69f1757222faf1d3bc3e381d8ca04795bdda
dhruvrajgupta <dhruv.learner@gmail.com>
2023-11-01 14:27:42 +0100

    Updating number of epochs to 50 from 2

--- src/refined/training/train/training_args.py
+++ src/refined/training/train/training_args.py
@@ -24,7 +24,7 @@ class TrainingArgs:
     ed_dropout: float = 0.05
     et_dropout: float = 0.10
     gradient_accumulation_steps: int = 1
-    epochs: int = 2
+    epochs: int = 50
     lr: float = 5e-5
     batch_size: int = 8  # 8 uses around 12 GB, 16 uses 22 GB (can save space if find GPU process allocating)
     ed_threshold: float = 0.15
